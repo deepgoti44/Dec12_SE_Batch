f3909d448b563518d67ea867ad0a144914c5e934
Arvindchocha <109568457+Arvindchocha@users.noreply.github.com>
2022-12-21 10:56:02 +0530

    h

---
+++ C Programming/Classwork/21-12-22/test1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	/*
+	//printf("C is a\t\t\tSea.\n");
+	printf("and not to See.");
+	sldm;lsdf
+	msdl;fsd
+	m,sdfl;
+	sdmfl;sdf
+	msdfl;
+	ls;dmfsdf
+	md;fl
+	msdl;fmsd;lf
+	*/
+}
---
+++ C Programming/Classwork/21-12-22/test2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("FName : Rakesh");
+	printf("\nLName : Kharva");
+	printf("\nAddress : Mumbai");
+	printf("\nD.O.B : 7 Sep 1992");
+}
